3d9b5b1cb7a0d828127e1fa4c7588b21612550ae
alexsharova <alexsharova@gmail.com>
2022-03-12 12:01:03 -0600

    Explicacion Html

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//es una espacio de memoria donde se amacena la informacion.
+//declaracion: es cuando se pone el nombre a un espacio en memoria
+let nombre;
+//asignacion: cuando de le asigna un valor a un espacio en memoria
+nombre = 'Juan';
+//tipo de datos: string, number, object, array,  boolran.
+//String: cadena de carateres
+let apellido = 'Perez';
+//Boolean: true o false
+let casado = false;
+//number: numeros
+let edad = 30;
+//Object: objetos. JSON (Javascript object notacion) SIntaxis:{propiedad: valor, propiedad: valor}
+let persona =  {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 30
+}
+//Array: Lista de datos
+let lenguajes = ['JavaScript', 'PHP', 'Python', 2, true, {nombre: 'Juan', apellido: 'Perez'}];
+
+//Javascript es un lenguaje no tipado
+//java es un lenguaje tipado
+//Tipado o no tipado = Que es necesario o no establecer el tipo de dato que va a guardar una variable
+
+//Ecmas = estandar de uso para javascript
+//EcmasScript = Es el estandar para uso de javascript
+//Typescript = Es la implementacion del estandar para uso de javascript
+// Tipado dinamico de datos
+//let nombre2: String;
+
+//Tipos de declaracion de variables en javascript
+//const: constante. No se puede cambiar el valor de la variable
+//var: variable. Se puede cambiar el valor de la variable, esto no es una variable global.
+//let: Variable. Se puede cambiar el valor de la variable, esto es una variable local. Depensde directamente de su contexto.
+//Existe dentro de una estructura de flujo determinada por un bloque de codigo.
